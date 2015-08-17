399952143e493322858702d5d367f672ee8e4415
Taruman <taru931@gmail.com>
2015-08-17 02:56:58 -0300

    instalando e configurando o OAuth2

---
+++ app/OAuth/Verifier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace angularavel\OAuth;
+
+use Illuminate\Support\Facades\Auth;
+
+class Verifier {
+  public function verify($username, $password)
+  {
+      $credentials = [
+        'email'    => $username,
+        'password' => $password,
+      ];
+
+      if (Auth::once($credentials)) {
+          return Auth::user()->id;
+      }
+
+      return false;
+  }
+}
