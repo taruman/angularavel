1d1b4e02d8094357a64a2995b00b3552060b602f
Taruman <taru931@gmail.com>
2015-08-21 02:26:43 -0300

    Criando ProjectMembersValidator e alterando ProjectService para usa-la, juntamente com a ProjectMembersRepository

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -2,16 +2,22 @@
 
 namespace angularavel\Services;
 use angularavel\Repositories\ProjectRepository;
+use angularavel\Repositories\ProjectMembersRepository;
 use \angularavel\Validators\ProjectValidator;
+use \angularavel\Validators\ProjectMembersValidator;
 use \Prettus\Validator\Exceptions\ValidatorException;
 
 class ProjectService {
     private $repository;
+    private $members_repository;
     private $validator;
+    private $members_validator;
     
-    public function __construct(ProjectRepository $repository, ProjectValidator $validator) {
+    public function __construct(ProjectRepository $repository, ProjectValidator $validator, ProjectMembersRepository $members_repository, ProjectMembersValidator $members_validator) {
         $this->repository = $repository;
+        $this->members_repository = $members_repository;
         $this->validator = $validator;
+        $this->members_validator = $members_validator;
     }
     
     public function create(array $data) {
@@ -41,4 +47,40 @@ class ProjectService {
         }      
               
     }
+    
+    function addMember(array $data) {   
+        
+        try {
+            $this->members_validator->with($data)->passesOrFail();
+            return $this->members_repository->create($data);
+        } catch (ValidatorException $exc) {
+            return [
+              "error" => true,
+              "message" => $exc->getMessageBag()
+            ];
+        }
+        
+    }
+    
+    function removeMember($id) {
+        
+        return $this->members_repository->delete($id);     
+              
+    }
+    
+    function isMember($id_user, $id_project) {
+        
+        $member = $this->members_repository->findWhere([
+            'user_id'=>$id_user,
+            'project_id'=>$id_project
+        ]); 
+        
+        if (count($member) > 0)
+        {
+            return true;
+        }
+        
+        return false;
+              
+    }    
 }
